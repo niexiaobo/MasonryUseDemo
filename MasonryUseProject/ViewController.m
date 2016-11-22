35fe07b2638b6f7d176d571f4639e83103b84d6b
niexiaobo <niexiaobo@niexiaobodeMacBook-Pro.local>
2016-11-22 12:12:26 +0800

    s

---
+++ MasonryUseProject/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  MasonryUseProject
+//
+//  Created by niexiaobo on 2016/11/22.
+//  Copyright © 2016年 niexiaobo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Podfile
@@ -0,0 +1,8 @@
+
+target 'MasonryUseProject' do
+
+platform :ios, '7.0'
+
+pod 'Masonry', '~> 1.0.2'
+pod 'BlocksKit', '~> 2.2.5'
+end
